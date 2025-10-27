f1b2c68c5a089acb43c68eebf12f574b607cbed6
vitusberinggg <virrebering@gmail.com>
2025-10-27 13:49:23 +0100

    reset remote controller code

---
+++ motor_controller.py
@@ -0,0 +1,15 @@
+
+# --- Imports ---
+
+import lgpio
+
+# --- Definitions ---
+
+
+
+# --- Setup ---
+
+handle = lgpio.gpiochip_open(0) # Opens GPIO controller 0 and returns a handle
+
+# --- Functions ---
+
