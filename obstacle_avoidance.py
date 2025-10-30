4bfd7d936dcbbc5576ebe5c063e57f380a53f064
vitusberinggg <virrebering@gmail.com>
2025-10-30 11:41:18 +0100

    moved helper functions

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -1,8 +1,96 @@
+
 # --- Imports ---
 
 import object_detection
 import time
 from motor_controller import forward, backwards, tank_turn_counterclockwise, tank_turn_clockwise, stop
+
+# --- Helper functions ---
+
+def check_left():
+
+    """
+    Turns slightly left to check that direction, then returns to original orientation.
+
+    Arguments:
+        None
+
+    Returns:
+        None
+    
+    """
+
+    tank_turn_counterclockwise()
+    time.sleep(0.4)  # adjust rotation
+    stop()
+    time.sleep(0.2)
+    tank_turn_clockwise()  # turn back
+    time.sleep(0.4)
+    stop()
+    time.sleep(0.2)
+
+def check_right():
+
+    """
+    Turns slightly right to check that direction, then returns to original orientation.
+    
+    Arguments:
+        None
+
+    Returns:
+        None
+    
+    """
+
+    tank_turn_clockwise()
+    time.sleep(0.4)
+    stop()
+    time.sleep(0.2)
+    tank_turn_counterclockwise()  # turn back
+    time.sleep(0.4)
+    stop()
+    time.sleep(0.2)
+
+
+def go_around_left():
+
+    """
+    Turns left and moves forward around the obstacle.
+    
+    Arguments:
+        None
+
+    Returns:
+        None
+    
+    """
+
+    tank_turn_counterclockwise()
+    time.sleep(0.6)
+    stop()
+    forward()
+    time.sleep(1.2)
+    stop()
+
+def go_around_right():
+
+    """
+    Turns right and moves forward around the obstacle.
+    
+    Arguments:
+        None
+
+    Returns:
+        None
+
+    """
+
+    tank_turn_clockwise()
+    time.sleep(0.6)
+    stop()
+    forward()
+    time.sleep(1.2)
+    stop()
 
 # --- Main function ---
 
@@ -48,82 +136,3 @@ def avoid_obstacle():
         backwards()
         time.sleep(1)
         avoid_obstacle()
-
-# --- Helper functions ---
-
-def check_left():
-
-    """
-    Turns slightly left to check that direction, then returns to original orientation.
-
-    Arguments: None
-
-    Returns: None
-    
-    """
-
-    tank_turn_counterclockwise()
-    time.sleep(0.4)  # adjust rotation
-    stop()
-    time.sleep(0.2)
-    tank_turn_clockwise()  # turn back
-    time.sleep(0.4)
-    stop()
-    time.sleep(0.2)
-
-def check_right():
-
-    """
-    Turns slightly right to check that direction, then returns to original orientation.
-    
-    Arguments: None
-
-    Returns: None
-    
-    """
-
-    tank_turn_clockwise()
-    time.sleep(0.4)
-    stop()
-    time.sleep(0.2)
-    tank_turn_counterclockwise()  # turn back
-    time.sleep(0.4)
-    stop()
-    time.sleep(0.2)
-
-
-def go_around_left():
-
-    """
-    Turns left and moves forward around the obstacle.
-    
-    Arguments: None
-
-    Returns: None
-    
-    """
-
-    tank_turn_counterclockwise()
-    time.sleep(0.6)
-    stop()
-    forward()
-    time.sleep(1.2)
-    stop()
-
-def go_around_right():
-
-    """
-    Turns right and moves forward around the obstacle.
-    
-    Arguments: None
-
-    Returns: None
-
-    """
-    
-    tank_turn_clockwise()
-    time.sleep(0.6)
-    stop()
-    forward()
-    time.sleep(1.2)
-    stop()
