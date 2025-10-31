4451ab5ad5edbf2a7b2bfbc2e688fc2d0ac5e3c4
klarapalsson <klara.palsson@ericsson.com>
2025-10-31 11:29:28 +0100

    commented obstacle avoidance so we can try without for now

--- main.py
+++ main.py
@@ -68,7 +68,8 @@ def follow():
 
         if obstacle: #or distance_in_cm <= safe_distance_in_cm: # If either the AI camera or the ultrasonic sensor detects an obstacle:
             print_and_log("Trying to avoid an obstacle...")
-            obstacle_avoidance.avoid_obstacle()
+            #obstacle_avoidance.avoid_obstacle()
+            stop()
             continue
             
         if person_area is None:
